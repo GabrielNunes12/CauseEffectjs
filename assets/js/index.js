7938cb1681bc02763bc0b14909200e37e4db571d
Gabriel Nunes <prismaticnetwork12@gmail.com>
2021-05-17 19:41:37 -0300

    [fix] refactor js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,41 @@
 const summary = document.getElementById('people').innerHTML;
 
-const people = {
-  name: "Joao", 
-  street: "joaooa", 
-  city: "joaooa", 
-  state: "joaooa", 
-  country: "joaooa", 
-  telephone: "joaooa", 
-  birthday: "joaooa"
-};
+const people = [
+  {
+    id: 1,
+    name: "joseph", 
+    street: "AJUd", 
+    city: "ggg", 
+    state: "bd", 
+    country: "gh", 
+    telephone: "999999", 
+    birthday: "2020202"
+  },
+  {
+    id: 2,
+    name: "Mary", 
+    street: "Mary", 
+    city: "Mary", 
+    state: "Mary", 
+    country: "Mary", 
+    telephone: "Mary", 
+    birthday: "Mary"
+  },
+  {
+    id: 3,
+    name: "Jocker", 
+    street: "AJA", 
+    city: "Jocker", 
+    state: "AAA", 
+    country: "Jocker", 
+    telephone: "Jocker", 
+    birthday: "Jocker"
+  }
+];
 
-document.getElementById('result').innerHTML = `Name: ${people.name} 
-                                               His birthday: ${people.birthday}
-                                               City: ${people.city} 
-                                               country: ${people.country}
-                                               State: ${people.state}
-                                               Street: ${people.street}`;
+
+const arrayPeople = people.map((value, index) => {
+  return `\n${value.name} \n ${value.telephone} \n ${value.street} \n ${value.state} \n ${value.city} \n ${value.birthday}`;
+})
+
+document.getElementById('result').innerText = `${arrayPeople}`;
